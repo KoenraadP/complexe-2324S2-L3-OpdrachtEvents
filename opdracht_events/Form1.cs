a217c4b75122ea778ba112e0246ebdace07b39c5
Koenraad Pecceu <koenraad.pecceu@student.howest.be>
2024-03-14 13:18:18 +0100

    Initial commit

---
+++ opdracht_events/Form1.cs
@@ -0,0 +1,41 @@
+﻿using opdracht_events.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace opdracht_events
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                pictureBox1.Image = Resources.runright;
+                pictureBox1.Left += 10;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                pictureBox1.Image = Resources.runleft;
+                pictureBox1.Left -= 10;
+            }
+        }
+    }
+}
